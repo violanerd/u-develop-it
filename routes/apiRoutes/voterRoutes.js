89f1d536263f46975cdc9f5d8a5c338763e960a0
violanerd <madalyne.cross@gmail.com>
2022-08-10 14:04:15 -0500

    added voter routes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -31,4 +31,67 @@ router.get('/voter/:id', (req, res) => {
         });
     });
 });
+
+router.post('/voter', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if (errors) {
+        res.status(400).json({ error: errors});
+        return;
+    }
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+    db.query(sql, params, (err, result) =>{
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+router.put('/voter/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+        res.status(400).json({ error: errors});
+        return;
+    }
+    const sql = 'UPDATE voters SET email = ? WHERE id = ?';
+    const params = [req.body.email, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ err: err.message });
+        } else if (!result.affectedRows){
+            res.json({ message: 'voter not found'});
+        } else {
+            res.json({
+                message: 'Success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+})
+
+router.delete('/voter/:id', (req, res) =>{
+    const sql = `DELETE FROM voters WHERE id = ?`;
+    
+    db.query(sql, req.params.id, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows){
+            res.json({
+                message: 'voter not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 module.exports = router;
